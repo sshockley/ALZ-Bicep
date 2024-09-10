a04be5b5b0f860c38f55d66a4ec01a74696f6eff
Steve Shockley <sshockley@aras.com>
2024-09-10 13:28:05 -0400

    Check in tests

--- Aras/testPolicyAssignment.sh
+++ Aras/testPolicyAssignment.sh
@@ -13,7 +13,19 @@ parPolicyAssignmentParameters+="\"ascExportResourceGroupLocation\":{\"value\":\"
 parPolicyAssignmentParameters+="\"enableAscForServers\":{\"value\":\"Enabled\"},"
 parPolicyAssignmentParameters+="\"enableAscForSql\":{\"value\":\"Enabled\"}}"
 
-parPolicyAssignmentDefinitionId="/providers/Microsoft.Management/managementGroups/${MANAGEMENTGROUP}/providers/Microsoft.Authorization/policySetDefinitions/Deploy-MDFC-Config"
+parTopLevelPolicyAssignmentSovereigntyGlobal="{"
+parTopLevelPolicyAssignmentSovereigntyGlobal+="\"parTopLevelSovereigntyGlobalPoliciesEnable\":{\"value\":false}"
+parTopLevelPolicyAssignmentSovereigntyGlobal+="\"parListOfAllowedLocations\":{\"value\":\"[]\"}"
+parTopLevelPolicyAssignmentSovereigntyGlobal+="\"parPolicyEffect\":{\"value\":\"Deny\"}"
+parTopLevelPolicyAssignmentSovereigntyGlobal+="}"
+
+parPolicyAssignmentSovereigntyConfidential="{"
+parPolicyAssignmentSovereigntyConfidential+="\"parAllowedResourceTypes\":{\"value\":false}"
+parPolicyAssignmentSovereigntyConfidential+="\"parListOfAllowedLocations\":{\"value\":\"[]\"}"
+parPolicyAssignmentSovereigntyConfidential+="\"parAllowedVirtualMachineSKUs\":{\"value\":\"[]\"}"
+parPolicyAssignmentSovereigntyConfidential+="\"parPolicyEffect\":{\"value\":\"Deny\"}"
+parPolicyAssignmentSovereigntyConfidential+="}"
+
 
 # ---
 
@@ -28,19 +40,68 @@ dateYMD=$(date +%Y%m%dT%H%M%S%NZ)
 NAME="alz-PolicyDineAssignments-${dateYMD}"
 #MGID="alz-landingzones"
 MGID="TMP-MGT-GRP"
-TEMPLATEFILE="infra-as-code/bicep/modules/policy/assignments/policyAssignmentManagementGroup.bicep"
-PARAMETERS="@infra-as-code/bicep/modules/policy/assignments/parameters/policyAssignmentManagementGroup.dine.parameters.all.json"
+#TEMPLATEFILE="infra-as-code/bicep/modules/policy/assignments/policyAssignmentManagementGroup.bicep"
+TEMPLATEFILE="infra-as-code/bicep/modules/policy/assignments/alzDefaults/mg-alzDefaultPolicyAssignments.bicep"
+#PARAMETERS="@infra-as-code/bicep/modules/policy/assignments/parameters/mg-policyAssignmentManagementGroup.dine.parameters.all.json"
+PARAMETERS="infra-as-code/bicep/modules/policy/assignments/alzDefaults/parameters/alzDefaultPolicyAssignments.parameters.all.json"
 
-#echo ${parPolicyAssignmentParameters}
+# POLICIES=$(grep 'name: ' infra-as-code/bicep/modules/policy/definitions/mg-customPolicyDefinitions.bicep | \
+#   cut -d':' -f2 | \
+#   grep " '" | \
+#   head -n -1 | \
+#   tr -d " '")
 
-az deployment mg create \
-  --name ${NAME} \
-  --location ${REGION} \
-  --management-group-id ${MGID} \
-  --template-file ${TEMPLATEFILE} \
-  --parameters ${PARAMETERS} \
-  --parameters parPolicyAssignmentParameters=${parPolicyAssignmentParameters} \
-  --parameters parPolicyAssignmentIdentityRoleAssignmentsAdditionalMgs="[]" \
-  --parameters parPolicyAssignmentDefinitionId=${parPolicyAssignmentDefinitionId}
+POLICIES="DenyAction-DeleteProtection"
 
-#parTags="${tags}"
+for policy in $POLICIES; do
+  echo "Doing ${policy}"
+
+  parPolicyAssignmentDefinitionId="/providers/Microsoft.Management/managementGroups/${MGID}/providers/Microsoft.Authorization/policySetDefinitions/${policy}"
+
+  az deployment mg validate \
+    --name ${NAME} \
+    --location ${REGION} \
+    --management-group-id ${MGID} \
+    --template-file ${TEMPLATEFILE} \
+    --parameters parTopLevelManagementGroupPrefix="alz" \
+        parTopLevelManagementGroupSuffix="" \
+        parLogAnalyticsWorkSpaceAndAutomationAccountLocation=${REGION} \
+        parLogAnalyticsWorkspaceLogRetentionInDays="365" \
+        parAutomationAccountName="alz-AutomationAccount" \
+        parMsDefenderForCloudEmailSecurityContact="sshockley@aras.com" \
+        parDisableAlzDefaultPolicies=false \
+        parLogAnalyticsWorkspaceResourceID="/subscriptions/a5e9fd1e-d16c-4dac-940c-a9d8b2215175/resourceGroups/rg-Sentinel-prod/providers/Microsoft.OperationalInsights/workspaces/la-Sentinel-prod-usgovvirginia" \
+        parTelemetryOptOut=true \
+    --debug
+
+"/subscriptions/a5e9fd1e-d16c-4dac-940c-a9d8b2215175/resourceGroups/rg-Sentinel-prod/providers/Microsoft.OperationalInsights/workspaces/la-Sentinel-prod-usgovvirginia"
+"/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/alz-logging/providers/Microsoft.OperationalInsights/workspaces/alz-log-analytics"
+        # Supported in gov but not used
+        #parDdosProtectionPlanId \
+
+
+        # Not supported in Gov
+#        parDdosEnabled=false \
+#        parPlatformMgAlzDefaultsEnable=true \
+#        parLandingZoneChildrenMgAlzDefaultsEnable=true \
+#        parLandingZoneMgConfidentialEnable=true \
+
+        # parDataCollectionRuleVMInsightsResourceId="/subscriptions/a5e9fd1e-d16c-4dac-940c-a9d8b2215175/resourceGroups/rg-Sentinel-prod/providers/Microsoft.Insights/dataCollectionRules/dcr-Sentinel-prod-usgovvirginia" \
+        # parDataCollectionRuleChangeTrackingResourceId="/subscriptions/a5e9fd1e-d16c-4dac-940c-a9d8b2215175/resourceGroups/rg-Sentinel-prod/providers/Microsoft.Insights/dataCollectionRules/dcr-Sentinel-prod-usgovvirginia" \
+        # parDataCollectionRuleMDFCSQLResourceId= \
+        # parUserAssignedManagedIdentityResourceId= \
+        # parPrivateDnsResourceGroupId \
+        # parPrivateDnsZonesNamesToAuditInCorp \
+        # parVmBackupExclusionTagName="" \
+        # parVmBackupExclusionTagValue="" \
+        # parExcludedPolicyAssignments="" \
+        # parTopLevelPolicyAssignmentSovereigntyGlobal=${parTopLevelPolicyAssignmentSovereigntyGlobal} \
+        # parPolicyAssignmentSovereigntyConfidential=${parPolicyAssignmentSovereigntyConfidential}
+
+
+    # --parameters ${PARAMETERS} \
+    # --parameters parPolicyAssignmentParameters=${parPolicyAssignmentParameters} \
+    # --parameters parPolicyAssignmentIdentityRoleAssignmentsAdditionalMgs="[]" \
+    # --parameters parPolicyAssignmentDefinitionId=${parPolicyAssignmentDefinitionId}
+done
+
